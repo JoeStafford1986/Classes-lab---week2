e05e4622f3c999380de07903af08a0e8bd3cfba7
Joe Stafford <joestafford1986@gmail.com>
2018-04-17 06:50:56 +0100

    added find_player_by_name

--- part_b/specs/sports_team_spec.rb
+++ part_b/specs/sports_team_spec.rb
@@ -6,23 +6,38 @@ class TestSportsTeam < MiniTest::Test
 
   def test_get_team_name
     sports_team = SportsTeam.new("Celtic", ["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], "Brendan Rodgers")
-    assert_equal("Celtic", sports_team.get_name())
+
+    assert_equal("Celtic", sports_team.team_name())
   end
 
   def test_get_players
     sports_team = SportsTeam.new("Celtic", ["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], "Brendan Rodgers")
-    assert_equal(["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], sports_team.get_players())
+
+    assert_equal(["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], sports_team.players())
   end
 
   def test_get_coach_name
     sports_team = SportsTeam.new("Celtic", ["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], "Brendan Rodgers")
-    assert_equal("Brendan Rodgers", sports_team.get_coach_name())
+
+    assert_equal("Brendan Rodgers", sports_team.coach_name())
   end
 
   def test_set_coach_name
     sports_team = SportsTeam.new("Celtic", ["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], "Brendan Rodgers")
-    sports_team.set_coach_name("Tommy Burns")
-    assert_equal("Tommy Burns", sports_team.get_coach_name())
+    sports_team.coach_name = "Tommy Burns"
+    assert_equal("Tommy Burns", sports_team.coach_name())
+  end
+
+  def test_add_player
+    sports_team = SportsTeam.new("Celtic", ["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], "Brendan Rodgers")
+    sports_team.add_player("Scott Brown")
+    assert_equal(["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele", "Scott Brown"], sports_team.players())
+  end
+
+  def test_find_player_by_name
+    sports_team = SportsTeam.new("Celtic", ["Jimmy Johnson", "Henrik Larsson", "Mousa Dembele"], "Brendan Rodgers")
+
+    assert_equal(true, sports_team.find_player_by_name("Henrik Larsson"))
   end
 
 end
--- part_b/sports_team.rb
+++ part_b/sports_team.rb
@@ -1,25 +1,22 @@
 class SportsTeam
 
+  attr_reader(:team_name, :players)
+  attr_accessor(:coach_name)
+
   def initialize(input_team_name, input_players, input_coach_name)
     @team_name = input_team_name
     @players = input_players
     @coach_name = input_coach_name
   end
 
-  def get_name()
-    return @team_name
+  def add_player(new_player)
+    @players.push(new_player)
   end
 
-  def get_players
-    return @players
+  def find_player_by_name(player_to_find)
+    for player in @players
+      return true if player == player_to_find
+    end
   end
-
-  def get_coach_name
-    return @coach_name
-  end
-
-  def set_coach_name(new_name)
-    @coach_name = new_name
-  end
-
+  
 end
